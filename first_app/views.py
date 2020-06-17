0b7789494e00228c05ad1c43cb0bd59049e6c4c4
siul1024 <hnong09@gmail.com>
2020-06-17 17:40:40 +0900

    update category list

--- first_app/views.py
+++ first_app/views.py
@@ -5,12 +5,29 @@ from django.shortcuts import render
 from first_app.models import Category, BuildingInfo, ShopInfo
 
 def index(request):
-    s_count = ShopInfo.objects.all().count()
-    korean_count = ShopInfo.objects.filter(cat_id='1').count()
+    all_count = ShopInfo.objects.all().count()
+    c1_count = ShopInfo.objects.filter(cat_id='1').count()
+    c2_count = ShopInfo.objects.filter(cat_id='2').count()
+    c3_count = ShopInfo.objects.filter(cat_id='3').count()
+    c4_count = ShopInfo.objects.filter(cat_id='4').count()
+    c5_count = ShopInfo.objects.filter(cat_id='5').count()
+    c6_count = ShopInfo.objects.filter(cat_id='6').count()
+    c7_count = ShopInfo.objects.filter(cat_id='7').count()
+    c8_count = ShopInfo.objects.filter(cat_id='8').count()
+    c9_count = ShopInfo.objects.filter(cat_id='9').count()
+
 
     context = {
-        's_count': s_count,
-        'korean_count': korean_count,
+        'all_count': all_count,
+        'c1_count': c1_count,
+        'c2_count': c2_count,
+        'c3_count': c3_count,
+        'c4_count': c4_count,
+        'c5_count': c5_count,
+        'c6_count': c6_count,
+        'c7_count': c7_count,
+        'c8_count': c8_count,
+        'c9_count': c9_count,
     }
 
     return render(request, 'index.html', context=context)
