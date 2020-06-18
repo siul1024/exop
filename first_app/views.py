96dda79c0b69cb48a89f398dccd3e5e131fd750d
siul1024 <hnong09@gmail.com>
2020-06-18 09:23:48 +0900

    update index

--- first_app/views.py
+++ first_app/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from first_app.models import Category, BuildingInfo, ShopInfo
+from first_app.models import ShopInfo
 
 def index(request):
     all_count = ShopInfo.objects.all().count()
+    # 분류별
     c1_count = ShopInfo.objects.filter(cat_id='1').count()
     c2_count = ShopInfo.objects.filter(cat_id='2').count()
     c3_count = ShopInfo.objects.filter(cat_id='3').count()
@@ -15,7 +16,18 @@ def index(request):
     c7_count = ShopInfo.objects.filter(cat_id='7').count()
     c8_count = ShopInfo.objects.filter(cat_id='8').count()
     c9_count = ShopInfo.objects.filter(cat_id='9').count()
-
+    # 건물별
+    b1_count = ShopInfo.objects.filter(bldg_id='1').count()
+    b2_count = ShopInfo.objects.filter(bldg_id='2').count()
+    b3_count = ShopInfo.objects.filter(bldg_id='3').count()
+    b4_count = ShopInfo.objects.filter(bldg_id='4').count()
+    b5_count = ShopInfo.objects.filter(bldg_id='5').count()
+    b6_count = ShopInfo.objects.filter(bldg_id='6').count()
+    b7_count = ShopInfo.objects.filter(bldg_id='7').count()
+    b8_count = ShopInfo.objects.filter(bldg_id='8').count()
+    b9_count = ShopInfo.objects.filter(bldg_id='9').count()
+    b10_count = ShopInfo.objects.filter(bldg_id='10').count()
+    b11_count = ShopInfo.objects.filter(bldg_id='11').count()
 
     context = {
         'all_count': all_count,
@@ -28,6 +40,17 @@ def index(request):
         'c7_count': c7_count,
         'c8_count': c8_count,
         'c9_count': c9_count,
+        'b1_count': b1_count,
+        'b2_count': b2_count,
+        'b3_count': b3_count,
+        'b4_count': b4_count,
+        'b5_count': b5_count,
+        'b6_count': b6_count,
+        'b7_count': b7_count,
+        'b8_count': b8_count,
+        'b9_count': b9_count,
+        'b10_count': b10_count,
+        'b11_count': b11_count,
     }
 
     return render(request, 'index.html', context=context)
