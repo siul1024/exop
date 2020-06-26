776eaabe9972f0314f0b4c561f6e0c688f4dbd22
siul1024 <hnong09@gmail.com>
2020-06-26 11:18:07 +0900

    add marker img

--- first_app/static/js/mapOption.js
+++ first_app/static/js/mapOption.js
@@ -13,16 +13,23 @@ window.onload = function(){
 
     obj_map = new naver.maps.Map('map', mapOptions);
 
-    for (var key in shop_info_list){
-        var position = new naver.maps.LatLng(shop_info_list[key].bldg_lat, shop_info_list[key].bldg_lng);
-        addMarker(position, key);
-    };
-
     var htmlMarker = {
         content: '<div style="cursor:pointer;width:50px;height:50px;line-height:52px;font-size:15px;color:white;text-align:center;font-weight:bold;background:url(/static/img/cluster-marker-2.png);background-size:contain;"></div>',
         size: new naver.maps.Size(40, 40),
         anchor: new naver.maps.Point(20, 20)
     };
+    var htmlMarker_blue = {
+        content: '<div style="cursor:pointer;width:35px;height:35px;line-height:52px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(/static/img/map-marker-1.png);background-size:contain;"></div>',
+        size: new naver.maps.Size(40, 40),
+        anchor: new naver.maps.Point(20, 20)
+    };
+
+    for (var key in shop_info_list){
+        var position = new naver.maps.LatLng(shop_info_list[key].bldg_lat, shop_info_list[key].bldg_lng);
+        addMarker_img(position, key, htmlMarker_blue);
+    };
+
+
 
     markerClustering = new MarkerClustering({
         minClusterSize: 2,
@@ -50,6 +57,21 @@ function addMarker(position, id){
     markers.push(marker);
     return marker;
 }
+
+
+function addMarker_img(position, id, html_marker){
+    var marker = new naver.maps.Marker({
+        position: position,
+        map: obj_map,
+        icon: html_marker,
+        title: shop_info_list[id].shop_name,
+        animation: naver.maps.Animation.BOUNCE,
+    });
+    marker.setMap(obj_map);
+    markers.push(marker);
+    return marker;
+}
+
 
 function removeAllMarker(){
     for( var i=0; i<markers.length; i++){
